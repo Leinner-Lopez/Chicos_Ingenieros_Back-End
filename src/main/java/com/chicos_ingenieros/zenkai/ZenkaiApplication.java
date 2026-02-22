ad6863fe8f4ed53ea9f77013ba70cfc68ddf51f2
Leinner-Lopez <leinner1006@gmail.com>
2026-02-21 21:18:37 -0500

    Primer Commit

---
+++ src/main/java/com/chicos_ingenieros/zenkai/ZenkaiApplication.java
@@ -0,0 +1,13 @@
+package com.chicos_ingenieros.zenkai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenkaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenkaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chicos_ingenieros/zenkai/ZenkaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chicos_ingenieros.zenkai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenkaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
