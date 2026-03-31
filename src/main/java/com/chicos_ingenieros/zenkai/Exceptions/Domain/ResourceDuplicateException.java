6e983b81a168c7608bae6ce479c3ed3a7f28a1ab
Leinner-Lopez <leinner1006@gmail.com>
2026-03-30 19:19:33 -0500

    feat: Se agregan las anotaciones correspondientes para la exception

--- src/main/java/com/chicos_ingenieros/zenkai/Exceptions/Domain/ResourceDuplicateException.java
+++ src/main/java/com/chicos_ingenieros/zenkai/Exceptions/Domain/ResourceDuplicateException.java
@@ -1,5 +1,12 @@
 package com.chicos_ingenieros.zenkai.Exceptions.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
 public class ResourceDuplicateException extends RuntimeException {
     private String message;
 }
